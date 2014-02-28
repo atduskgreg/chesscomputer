59dd2aa00eaa29061022dc9ff6217d5a19362604
Shannon Kao <skao@mit.edu>
2014-02-28 01:07:10 -0500

    initial commit

---
+++ autoplay.py
@@ -0,0 +1,76 @@
+import subprocess, time, threading, Queue, re
+
+
+class Uci:
+    engine = subprocess.Popen(
+        'stockfish.exe',
+        universal_newlines=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    
+    def send(self, command):
+        print(command)
+        self.engine.stdin.write(command+'\n')
+
+    def nextMove():
+        return 0
+
+        
+
+uci = Uci()
+gameString = ""
+centipawns = 0
+whiteCP = []
+blackCP = []
+
+white = True
+
+q = Queue.Queue()
+class Listener(threading.Thread):
+    def run(self):
+        while True:
+            out = uci.engine.stdout.readline().strip()
+            if out != '':
+                q.put(out)
+            time.sleep(0.25)
+t = Listener()
+t.start()
+
+def scoreString(scores):
+    string = ""
+    for i in range(0, len(scores)):
+        string += ' ' + str(scores[i])
+    return string
+
+while True:
+    res = q.get()
+    print('     '+res)
+    if len(gameString) > 5*20: # 20 moves
+        print('\nMoves: ' + gameString)
+        print('White: ' + scoreString(whiteCP))
+        print('Black: ' + scoreString(blackCP))
+        break
+    if re.search('^Stockfish', res):
+        uci.send("uci")
+    if re.search('^uciok', res):
+        uci.send("isready")
+    if re.search('^readyok', res):
+        uci.send("ucinewgame")
+        uci.send("position startpos")
+        uci.send("go infinite")
+    if re.search('depth (?P<depth>\d+) seldepth \d+ score cp (?P<cp>-?\w+)', res):
+        info = re.search('depth (?P<depth>\d+) seldepth \d+ score cp (?P<cp>-?\w+)', res)
+        if int(info.group('depth')) > 5: # depth 5
+            centipawns = int(info.group('cp'))
+            uci.send("stop")
+    if re.search('^bestmove (?P<move>\w+) ', res):
+        if white:
+            whiteCP.append(centipawns)
+        else:
+            blackCP.append(centipawns)
+        white = not white
+        bestmove = re.search('^bestmove (?P<move>\w+) ', res)
+        gameString += " " + (bestmove.group('move'))
+        uci.send("position startpos moves" + gameString)
+        uci.send("go infinte")
---
+++ chessviz/index.js
@@ -0,0 +1,184 @@
+var rootImageURL = "images";
+var startPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+var moveString = "";
+var moves = [];
+var currMove = 0;
+
+var eatenPieces = [];
+
+var pos = {
+	"a" : 0,
+	"b" : 1,
+	"c" : 2,
+	"d" : 3,
+	"e" : 4,
+	"f" : 5,
+	"g" : 6,
+	"h" : 7,
+}
+//e2e4 d2d4 g8f6 g1f3 e7e6 e2e3 f8e7 b1c3 e8g8 f1e2 d7d5 e1g1 b8c6 c1d2 c8d7 f3e5 c6e5 d4e5 f6e4 c3e4 d5e4
+
+$( document ).ready(function() {
+	black = false;
+
+	for(var row = 0; row < 8; row++){
+		$("#board table").append("<tr></tr>");
+		for(var col = 0; col < 8; col++){
+			var c;
+			if(black){
+				c = "black";
+			} else {
+				c = "white";
+			}
+			$("#board table tr:last").append("<td class='"+c+"' id='"+col+"_"+row+"'></td>");
+			black = !black;
+
+			if(col == 7){
+				black = !black;
+			}
+		}
+	}
+
+	displayPosition( startPos );
+	
+	function switchPlayer() {
+		if (black) {
+			$('#turn').html("BLACK");
+		} else {
+			$('#turn').html("WHITE");
+		}
+		black = !black;
+	}
+	function moveForward(move){
+		$("#board td").css("outline", "none");
+		from = pos[move.charAt(0)]+"_"+(8-parseInt(move.charAt(1)));
+		to = pos[move.charAt(2)]+"_"+(8-parseInt(move.charAt(3)));
+		console.log(from + " " + to);
+		
+		piece = $("#"+from).html();
+		$("#"+from).html('').css("outline", "4px solid blue");
+		$("#"+to).html(piece).css("outline", "4px solid red");
+	}
+	
+	function moveBack(move){
+		$("#board td").css("outline", "none");
+		from = pos[move.charAt(0)]+"_"+(8-parseInt(move.charAt(1)));
+		to = pos[move.charAt(2)]+"_"+(8-parseInt(move.charAt(3)));
+		console.log(to + " " + from);
+		
+		piece = $("#"+to).html();
+		$("#"+to).html('');
+		$("#"+from).html(piece);
+		
+		prevMove = moves[currMove-1];
+		prevFrom = pos[prevMove.charAt(0)]+"_"+(8-parseInt(prevMove.charAt(1)));
+		prevTo = pos[prevMove.charAt(2)]+"_"+(8-parseInt(prevMove.charAt(3)));
+		$("#"+prevTo).css("outline", "4px solid red");
+		$("#"+prevFrom).css("outline", "4px solid blue");
+	}
+	
+	$("#display").click(function() {
+		var fenString = $('#f').val();
+		display.Position(fenString);
+	});
+	
+	$("#start").click(function() {
+		displayPosition( startPos );
+		$('#turn').html("WHITE");
+		moveString = $('#g').val();
+		moves = moveString.split(" ");
+		$('#moves').html(moveString);
+		currMove = 0;
+		$('#currMove').html(moves[currMove]);
+		moveForward(moves[currMove]);
+	});
+	
+	$("#prev").click(function() {
+		if (currMove>0){
+			moveBack(moves[currMove]);
+			currMove -= 1;
+			$('#currMove').html(moves[currMove]);
+			switchPlayer();
+		}
+	});
+	
+	$("#next").click(function() {
+		if (currMove<moves.length-1){
+			currMove += 1;
+			$('#currMove').html(moves[currMove]);
+			moveForward(moves[currMove]);
+			switchPlayer();
+		}
+	});
+	
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+	for(var i = 0; i < 8; i++){
+		$("#colSelect").append("<option value='"+letters[i]+"'>"+letters[i]+"</option>");
+		$("#rowSelect").append("<option value='"+i+"'>"+(i+1)+"</option>");
+	}
+	
+	$("#highlight").submit(function(){
+		$("#board td").css("outline", "none");
+
+		col = letters.indexOf($("#colSelect").val());
+		row = $("#rowSelect").val();
+		console.log(col+"_"+row);
+		$("#"+col+"_"+row).css("outline", "4px solid red");
+		$("#clear-highlight").show();
+		return false;
+	});
+
+	$("#clear-highlight").click(function(){
+		$("#board td").css("outline", "none");
+		$(this).hide();
+		return false;
+	});
+});
+
+
+
+
+
+
+function displayPosition( fen_position ){
+	console.log(fen_position);
+	$(".pieceImg").hide();
+	parts = fen_position.split(" ");
+	//updateTurn(parts[1]);
+
+	rows = parts[0].split("/");
+
+	for(var row = 0; row < rows.length; row++){
+		if(rows[row] == "8"){ // empty row
+			continue;
+		} else {
+			chars = rows[row].split("");
+			// if(cols.length != 8){
+			// 	logError("row " + (row+1) + " is the wrong length: " + cols.length);
+			// }
+
+			var colNum = 0;
+			for(var c = 0; c < chars.length; c++){
+				if(chars[c].match(/\d/)){
+					numSkips = parseInt(chars[c]);
+					colNum += numSkips;
+				} else {
+					cellId = colNum + "_" + row;
+
+					var pieceColor = "black";
+					if(chars[c] == chars[c].toUpperCase()){
+						pieceColor = "white";
+					}
+
+					imageURL = rootImageURL + "/" + pieceColor + "/" + chars[c] + ".png";
+					$("#" + cellId).html("<img class='pieceImg' src='" + imageURL + "'/>");
+					colNum++;
+				}
+			}
+		}
+	}
+
+	//$("#fen-input textarea").val(fen_position);
+	//logMsg("board loaded from fen:\n" + fen_position);
+
+}
