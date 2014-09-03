e30a2afebfa6ecb987f967bc9c0bf1efeeda7d53
Greg Borenstein <greg.borenstein@gmail.com>
2014-09-03 11:29:27 -0400

    debug cloudmailin data structure

--- pgn_email/app.rb
+++ pgn_email/app.rb
@@ -9,6 +9,9 @@ get "/" do
 end
 
 post "/pgn_email" do
-	pgn = PGN.parse(params["attachements"]["0"][:tempfile].read)
+	puts params.keys
+	puts params["attachments"].keys
+	puts params["attachments"]["0"].keys
+	pgn = PGN.parse(params["attachments"]["0"][:tempfile].read)
 	puts pgn.inspect
 end
