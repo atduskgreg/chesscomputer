57157fc5cadecef94ed6a8b2d0aabaf878894349
Greg Borenstein <greg.borenstein@gmail.com>
2014-09-03 11:31:42 -0400

    receiving and parsing pgn over email works so remove extraneous debugging

--- pgn_email/app.rb
+++ pgn_email/app.rb
@@ -9,9 +9,6 @@ get "/" do
 end
 
 post "/pgn_email" do
-	puts params.keys
-	puts params["attachments"].keys
-	puts params["attachments"]["0"].keys
 	pgn = PGN.parse(params["attachments"]["0"][:tempfile].read)
 	puts pgn.inspect
 end
