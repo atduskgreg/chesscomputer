b49a9cd030340b2a2d0ca47b9ed3cce6aa25488b
Greg Borenstein <greg.borenstein@gmail.com>
2014-09-08 10:39:19 -0400

    mockup for live game interface

--- pgn_email/app.rb
+++ pgn_email/app.rb
@@ -3,9 +3,26 @@ require 'bundler/setup'
 require 'sinatra'
 require 'pgn'
 
+helpers do
+	def html_board(position)
+		result = "<table>"
+		position.inspect.split(/\n/).each do |row| 
+			result << "<tr>"
+			row.split("").each do |square|
+				result << "<td>#{square}</td>" 
+			end
+			result << "</tr>"
+		end
+		result << "</table>"
+		result 
+	end
+
+end
 
 get "/" do
-	"pgn email receiver"
+	# fake games for testing
+	@games = (0..20).collect{ PGN::Game.new([])}
+	erb :index
 end
 
 post "/pgn_email" do
