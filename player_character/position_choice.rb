d2016e551d57fdaf891aefb01f2d2b1dd3c9e28a
Greg Borenstein <greg.borenstein@gmail.com>
2014-09-18 17:22:07 -0400

    working on infrastructure to establish position choice baseline for user t-test

--- player_character/position_choice.rb
+++ player_character/position_choice.rb
@@ -3,12 +3,12 @@ if ARGV.length < 1
 	exit
 end
 
-ARGV = ["../../data/players/ASHLEY.PGN"]
-
 require 'pathname'
 require './position_analysis'
 require 'bundler/setup'
 require 'pgn'
+require 'descriptive_statistics'
+
 
 STOCKFISH_PATH = Pathname(__FILE__).parent + "bin" + "stockfish"
 
@@ -23,14 +23,42 @@ games = PGN.parse(open(ARGV[0]).read)
 
 pa = PositionAnalysis.new STOCKFISH_PATH.to_s
 
-game = games[0]
 
-pa.compare(game.positions[11].to_fen, game.positions[12].to_fen, :side => :black)
 
-# player = (game.tags["Black"] =~ player_regex) ? :black : :white
+terms = ["Material", "Imbalance", "Pawns", "Knights", "Bishops", "Rooks", "Queens", "Mobility", "King safety", "Threats", "Passed pawns", "Space", "Total"]
+result = {}
+terms.each{|term| result[term] = {:eg => [], :mg => []}}
 
-# game.positions.each_with_index do |pos, i|
-# 	if (pos.player == player) && (i < game.positions.length - 1)
 
-# 	end
-# end
+games[0..50].each_with_index do |game,i|
+puts i	
+	player = (game.tags["Black"] =~ player_regex) ? :black : :white
+	
+	game.positions.each_with_index do |pos, i|
+	 	if (pos.player == player) && (i < game.positions.length - 2) # there's a next move to compare to
+			r = pa.compare(game.positions[i].to_fen, game.positions[i+1].to_fen, :side => player)
+			terms.each do |term|
+				result[term][:mg] << r[term][:mg]
+				result[term][:eg] << r[term][:eg]
+			end
+	 	end
+	end
+
+end
+
+result.each do |term,values|
+
+	puts term
+	puts "mid-game mean: #{values[:mg].mean}"
+	puts "end-game mean: #{values[:eg].mean}"
+
+	# puts
+	# puts "mid-game values:"
+	# puts values[:mg].inspect
+	# puts
+	# puts "end-game values"
+	# puts values[:eg].inspect
+	# puts
+	# puts
+	puts
+end
