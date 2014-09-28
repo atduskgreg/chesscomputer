84fe6b140043cacdf333d614a827334dfa581d3c
Greg Borenstein <greg.borenstein@gmail.com>
2014-09-28 14:30:10 -0400

    working on implementing the elo odds polyfit in ruby

--- millionaire_chess/models.rb
+++ millionaire_chess/models.rb
@@ -3,18 +3,27 @@ require 'bundler/setup'
 require 'pgn'
 
 class Game
-	attr_accessor :pgn
-	attr_accessor :analysis
+	attr_accessor :pgn, :analysis, :white, :black
 
 	def initialize fen
 		@pgn = PGN::Game.new([])
 		@pgn.instance_variable_set("@positions", [PGN::FEN.new(fen).to_position])
-		
-
 		@analysis = Stockfish.analyze(fen)
+
+		white = Player.new
+		white.elo = 2750 + 2750 + rand(-150..150)
+
+		black = Player.new
+		black.elo = 2750 + 2750 + rand(-150..150)
+
 	end
 
 	def score
-		
+		{:white => (@analysis[:white_victory_odds]*100).round,
+		 :black => ((1 - @analysis[:white_victory_odds])*100).round}
 	end
+end
+
+class Player
+	attr_accessor :elo
 end
