986fb23d61f0f18bb1df70878213492f3ae2dfac
Greg Borenstein <greg.borenstein@gmail.com>
2014-10-03 14:10:45 -0400

    machinery for finding sacrifices in a batch of games. also first database scheme for games for tournament system

--- millionaire_chess/models.rb
+++ millionaire_chess/models.rb
@@ -1,28 +1,88 @@
 require './stockfish'
 require 'bundler/setup'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-migrations'
+# require 'dm-aggregates'
 require 'pgn'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/millionaire_chess")
+
+
 class Game
-	attr_accessor :pgn, :analysis, :white, :black
+  	WHITE_VICTORY = "1-0"
+  	BLACK_VICTORY = "0-1"
+  	DRAW = "1/2-1/2"
+  	UNKNOWN = "*"
 
-	def initialize fen
-		@pgn = PGN::Game.new([])
-		@pgn.instance_variable_set("@positions", [PGN::FEN.new(fen).to_position])
-		@analysis = Stockfish.analyze(fen)
+	include DataMapper::Resource
+  
+  	property :id, Serial
+  	property :pgn, Object
+  	property :event, String
+  	property :final_material, Object
+  	property :is_sacrifice, Boolean
+  	timestamps :at
+  	
+  	# TODO: store the actual PGN file somewhere in case something goes wrong
+  	# or just have them email it to me
+  	def self.load_batch options={}
+  		pgn = PGN.parse(options[:pgn_string])
+  		pgn.each do |game|
+  			g = Game.create :pgn => game, :event => options[:event]
+  			g.check_sacrifice!
+  		end
+  	end
 
-		@white = Player.new
-		@white.elo = 2750 + rand(-150..150)
+  	def check_sacrifice!
+  		position = pgn.positions.last
+  		score = Game.score_for_pgn_position(position)
+  		margin_of_victory = score[:white] - score[:black]
+  		sacrifice = false
 
-		@black = Player.new
-		@black.elo = 2750 + rand(-150..150)
+  		if margin_of_victory.abs > 3
+			if pgn.result == Game::WHITE_VICTORY && margin_of_victory < 0
+				sacrifice = true
+			end
+			if pgn.result == Game::BLACK_VICTORY && margin_of_victory > 0
+				sacrifice = true
+			end
+		end
 
-	end
+		self.is_sacrifice = sacrifice
+		self.final_material = score
+		self.save
+  	end
 
-	def score
-		{:white => (@analysis[:white_victory_odds]*100).round,
-		 :black => ((1 - @analysis[:white_victory_odds])*100).round}
-	end
+
+	def description
+  	  "#{pgn.tags["White"]} (White) v. #{pgn.tags["Black"]} (Black) at #{pgn.tags["Event"]}, #{pgn.tags["Site"]}, #{pgn.tags["Date"]}"
+  	end
+
+  	def self.score_for_pgn_position(position)
+  	  result = {:black => 0, :white => 0, :black_missing => [], :white_missing => []}
+	
+  	  values = {"p" => 1, "b" => 3, "n" => 3, "r" => 5, "q" => 9}
+	
+  	  position.board.squares.flatten.each do |square|
+  	    if square && values.keys.include?(square.downcase)
+  	      piece_value = values[square.downcase]
+	
+  	      if square == square.downcase # black in FEN
+  	        result[:black] = result[:black] + piece_value
+  	        result[:black_missing] << square
+  	      else # white
+  	        result[:white] = result[:white] + piece_value
+  	        result[:white_missing] << square
+  	      end
+  	    end
+  	  end
+	
+  	  return result
+  	end
 end
+
+DataMapper.finalize
 
 class Player
 	attr_accessor :elo
